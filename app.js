768feb097da97be419369fdbc0aec2dfffaf947d
Bryson Creighton <brysoncreighton@gmail.com>
2020-05-26 17:53:47 -0600

    Complete server backend with sorting of books

--- app.js
+++ app.js
@@ -5,8 +5,40 @@ const app = express();
 
 app.use(morgan('common')); // let's see what common format looks like
 
-app.get('/books', (req, res) => {
+const books = require('./books-data.js');
 
+app.get('/books', (req, res) => {
+  const { search = '', sort } = req.query;
+
+  // Check for valid sort parameter
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of title or rank');
+    }
+  }
+
+  // Search Query
+  let results = books
+    .filter(book =>
+      book
+        .title
+        .toLowerCase()
+        .includes(search.toLocaleLowerCase()));
+
+  if (sort) {
+    results
+      .sort((a, b) => {
+        return a[sort] > b[sort]
+          ? 1
+          : a[sort] < b[sort]
+            ? -1
+            : 0;
+      })
+  }
+
+  res.json(results);
 });
 
 app.listen(8000, () => {
