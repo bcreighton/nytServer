cbaa425c7d625134b7cac1a60a5e5eb11ceda2ba
Bryson Creighton <brysoncreighton@gmail.com>
2020-05-26 20:38:06 -0600

    Update app with client side form to a accept search

--- nytclient/src/App.js
+++ nytclient/src/App.js
@@ -23,8 +23,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-      </div>
+      <main className="App">
+        <h1>NYT Best Sellers</h1>
+        <div className='search'>
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <label htmlFor='search'>Search: </label>
+            <input
+              type='text'
+              id='search'
+              name='search'
+              value={this.state.search}
+              onChcnage={e => this.setSearch(e.target.value)}
+            />
+
+            <label htmlFor='sort'>Sort: </label>
+            <select
+              id='sort'
+              name='sort'
+              onChange={e => this.setSort(e.target.value)}
+            >
+              <option value=''>None</option>
+              <option value='title'>Title</option>
+              <option value='rank'>Rank</option>
+            </select>
+            <button type='submit'>Search</button>
+          </form>
+
+          <div className='App_error'>{this.state.error}</div>
+        </div>
+        {books}
+      </main>
     )
   }
 }
